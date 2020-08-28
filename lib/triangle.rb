903f1a963e3d996c9b49d75c87e1f47b7f5d36be
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-28 05:43:14 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,11 +14,11 @@ class Triangle
   def kind 
     if !triangle
       raise TriangleError
-    elsif @side1 == @side2 && @side1== @side3
+    elsif side1 == side2 && side1== side3
       :equilateral
-    elsif @side1 == @side2 || @side1==@side3 || @side2==@side3
+    elsif side1 == side2 || side1==side3 || side2==side3
       :isosceles
-    elsif @side1 != !@side2 && @side1!= @side3 && @side2!=@side3
+    elsif side1 != !side2 && side1!= side3 && side2!=side3
       :scalene
     end
   end  
